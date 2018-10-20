f2e26cb76cc003fe5b210cfc2031d25fec7d1ef4
Stefan Svinciak <stefan.svinciak@gmail.com>
2018-10-20 19:32:32 +0200

    uncomplete bunch of stuff

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,29 +1,46 @@
 import * as React from 'react';
-import {SendForm} from './SendForm';
-import {MessageList} from './MessageList';
-import {Sidebar} from '../channels/Sidebar';
+import {ProfilePicture} from '../common/ProfilePicture';
+// import {SendForm} from './SendForm';
+// import {MessageList} from './MessageList';
+// import {Sidebar} from '../channels/Sidebar';
+
+import '../../css/chat.css';
 
 interface Props {
     eventHandler: any;
 }
 
-
 interface ChatState {
 
 }
 
-
 export class Chat extends React.PureComponent<Props, ChatState> {
-
     render() {
         return (
-            <div id="container" className="app">
-                <Sidebar/>
-                <section id="main">
-                    {/*<Title />*/}
-                    <MessageList/>
-                    <SendForm/>
-                </section>
+            <div>
+                <nav className="navbar navbar-dark bg-primary">
+                    <div className="container">
+                        <span className="navbar-brand mb-0 h1">PV247</span>
+                        <ul className="nav navbar-nav navbar-right">
+                            <li className="nav-item dropdown">
+                                <div className="nav-link dropdown-toggle caret-off" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"/>
+                                <ul className="dropdown-menu">
+                                    <li className="dropdown-item">Profile</li>
+                                    <li className="dropdown-item">Logout</li>
+                                </ul>
+                            </li>
+                            <ProfilePicture/>
+                        </ul>
+                    </div>
+                </nav>
+                {/*<div id="container" className="app">*/}
+                {/*<Sidebar/>*/}
+                {/*<section id="main">*/}
+                {/*/!*<Title />*!/*/}
+                {/*<MessageList/>*/}
+                {/*<SendForm/>*/}
+                {/*</section>*/}
+                {/*</div>*/}
             </div>
         );
     }
