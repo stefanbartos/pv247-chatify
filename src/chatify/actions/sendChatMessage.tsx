ea142b84696eb4973aa61f67d1529251c534d1f6
Stefan Svinciak <stefan.svinciak@gmail.com>
2018-11-13 23:59:03 +0100

    fixed sending message bug

--- src/chatify/actions/sendChatMessage.tsx
+++ src/chatify/actions/sendChatMessage.tsx
@@ -5,7 +5,9 @@ import {CHATIFY_MESSAGE_SEND} from '../constants/actionTypes';
 export const sendChatMessage = (chatMessage: string): Action => ({
     type: CHATIFY_MESSAGE_SEND,
     payload: {
-        channelUuid: uuid(),
-        text: chatMessage,
+        id: uuid(),
+        chatMessageText: chatMessage,
     }
 });
+
+// TODO: move to actionCreators.ts
