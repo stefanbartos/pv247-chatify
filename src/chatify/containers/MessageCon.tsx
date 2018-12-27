7fdccd2a7d61deb22c7342d51b3f67d36f32b812
Stefan Svinciak <stefan.svinciak@gmail.com>
2018-12-27 12:10:24 +0100

    reformat file: added spaces

--- src/chatify/containers/MessageCon.tsx
+++ src/chatify/containers/MessageCon.tsx
@@ -1,9 +1,9 @@
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
-import {IState} from '../../common/IState';
-import {IMessageDispatchProps, IMessageStateProps, Message, MessageProps} from '../components/chat/Message';
-import {IChatMessage} from '../models/IChatMessage';
-import {sendChatMessage} from '../actions/sendChatMessage';
+import { IState } from '../../common/IState';
+import { IMessageDispatchProps, IMessageStateProps, Message, MessageProps } from '../components/chat/Message';
+import { IChatMessage } from '../models/IChatMessage';
+import { sendChatMessage } from '../actions/sendChatMessage';
 
 const mapStateToProps = (state: IState, props: MessageProps) => {
     return {
@@ -14,7 +14,7 @@ const mapStateToProps = (state: IState, props: MessageProps) => {
 
 const mapDispatchToProps = (dispatch: Dispatch) => {
     return {
-       onSend: (message: string) => dispatch(sendChatMessage(message))
+        onSend: (message: string) => dispatch(sendChatMessage(message))
     };
 };
 
--- src/chatify/reducers/channels.ts
+++ src/chatify/reducers/channels.ts
@@ -1,13 +1,13 @@
 import * as Immutable from 'immutable';
-import {IChannelItem} from '../models/IChannelItem';
-import {CHATIFY_CHANNEL_ADD} from '../constants/actionTypes';
+import { IChannelItem } from '../models/IChannelItem';
+import { CHATIFY_CHANNEL_ADD } from '../constants/actionTypes';
 
 export const channels = (prevState = Immutable.List<IChannelItem>(), action: Action): Immutable.List<IChannelItem> => {
     switch (action.type) {
         case CHATIFY_CHANNEL_ADD: {
-            const {id, name} = action.payload;
+            const { id, name } = action.payload;
 
-            return prevState.push({id, name});
+            return prevState.push({ id, name });
         }
         default:
             return prevState;
