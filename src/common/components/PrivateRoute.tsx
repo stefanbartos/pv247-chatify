30fe686b8c25f70d572a54328448b6ebe96737cd
Stefan Svinciak <stefan.svinciak@gmail.com>
2018-12-30 21:42:45 +0100

    extended redirection state if user is not authenticated

--- src/common/components/PrivateRoute.tsx
+++ src/common/components/PrivateRoute.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Route, RouteProps, Redirect, RouteComponentProps } from 'react-router';
+import * as routes from '../../chatify/constants/routes';
 
 interface PrivateRouteProps extends RouteProps {
     component: React.ComponentType<RouteComponentProps<any>> | React.ComponentType<any>;
@@ -9,10 +10,17 @@ export const PrivateRoute: React.StatelessComponent<PrivateRouteProps> = ({ comp
     <Route
         {...rest}
         render={componentProps =>
-            false
+            // TODO: replace false with information that user is authenticated
+            true
                 ? <Component {...componentProps} />
                 : <Redirect
-                    to={{ pathname: '/login' }}
+                    to={{
+                        pathname: routes.LOGIN,
+                        state: {
+                            from: componentProps.location,
+                            requiresAuth: true
+                        }
+                    }}
                 />
         } />
 );
