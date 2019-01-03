bc99b38a70690ee4dfc917525e0b64e4c644c5c4
Stefan Svinciak <stefan.svinciak@gmail.com>
2019-01-03 20:27:57 +0100

    added profile user details action creators with constants

--- src/chatify/actions/profile/actionCreators.ts
+++ src/chatify/actions/profile/actionCreators.ts
@@ -1,9 +1,17 @@
 import * as actionTypes from '../../constants/actionTypes';
 import { IUserDetails } from '../../models/IUserDetails';
 
-export const updateProfileUserDetails = (userDetails: IUserDetails): Action => ({
-    type: actionTypes.CHATIFY_PROFILE_UPDATE_USER_DETAILS,
+export const updateProfileUserDetailsSuccess = (userDetails: IUserDetails): Action => ({
+    type: actionTypes.CHATIFY_PROFILE_UPDATE_USER_DETAILS_SUCCESS,
     payload: {
         userDetails
     }
+});
+
+export const updateProfileUserDetailsFailed = (): Action => ({
+    type: actionTypes.CHATIFY_PROFILE_UPDATE_USER_DETAILS_FAILED,
+});
+
+export const updateProfileUserDetailsStarted = (): Action => ({
+    type: actionTypes.CHATIFY_PROFILE_UPDATE_USER_DETAILS_STARTED,
 });
