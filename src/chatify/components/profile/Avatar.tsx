503a4a5f4f543a6c2c4d99752580f2403ad48d5a
Stefan Svinciak <stefan.svinciak@gmail.com>
2019-01-04 17:56:40 +0100

    used AvatarImage in Avatar component + small enhancement

--- src/chatify/components/profile/Avatar.tsx
+++ src/chatify/components/profile/Avatar.tsx
@@ -1,28 +1,52 @@
 import * as React from 'react';
-import * as Picture from '../../images/default-profile.png';
+import { AvatarImage } from './AvatarImage';
 
-export class Avatar extends React.PureComponent {
+interface IAvatarState {
+    selectedFile?: File;
+    isFileSelected: boolean;
+}
+
+export class Avatar extends React.PureComponent<null, IAvatarState> {
+    readonly state: IAvatarState = {
+        isFileSelected: false
+    };
+
     private onFileSelect = (e: React.ChangeEvent<HTMLInputElement>): void => {
         if (e.target.files === null) {
+            this.setState(() => ({
+                isFileSelected: false,
+                selectedFile: undefined
+            }));
             return;
         }
         const inputFile = e.target.files[0];
 
         this.setState(() => ({
-            selectedFile: inputFile
+            selectedFile: inputFile,
+            isFileSelected: true
         }));
     }
 
-    private onFileUpload = (e: any): void => {
+    private onFileUpload = (e: React.MouseEvent<HTMLButtonElement>): void => {
         console.log(e);
     }
 
     render() {
         return (
             <div className="container avatar">
-                <img src={Picture} />
-                <input type="file" onChange={this.onFileSelect} />
-                <button onClick={this.onFileUpload}>Upload</button>
+                <AvatarImage url={'https://www.w3schools.com/w3css/img_lights.jpg'} />
+                <div>
+                    <input type="file" onChange={this.onFileSelect} />
+                </div>
+                <br />
+                <div>
+                    <button
+                        className="btn btn-primary"
+                        onClick={this.onFileUpload}
+                        disabled={this.state.isFileSelected}>
+                        Upload avatar
+                    </button>
+                </div>
             </div>
         );
     }
