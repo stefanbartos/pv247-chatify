7827cb98b438dc12c455787ba9ffe1a2557142da
Stefan Svinciak <stefan.svinciak@gmail.com>
2019-01-04 15:04:24 +0100

    used new avatar component in profile component

--- src/chatify/components/profile/Profile.tsx
+++ src/chatify/components/profile/Profile.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import * as Picture from '../../images/default-profile.png';
 import { UserDetailsContainer } from '../../containers/profile/UserDetailsContainer';
 import { ClipLoader } from 'react-spinners';
+import { AvatarContainer } from '../../containers/profile/AvatarContainer';
 
 export interface IProfileStateProps {
     readonly isFetchingUserDetails: boolean;
@@ -16,32 +16,18 @@ export class Profile extends React.PureComponent<IProfileDispatchProps & IProfil
         selectedFile: null
     };
 
-    private onFileSelect = (e: React.ChangeEvent<HTMLInputElement>): void => {
-        if (e.target.files === null) {
-            return;
-        }
-        const inputFile = e.target.files[0];
-
-        this.setState(() => ({
-            selectedFile: inputFile
-        }));
-    }
-
-    private onFileUpload = (e: any): void => {
-        console.log(e);
-    }
-
     componentDidMount() {
         this.props.fetchUserProfile();
     }
 
     render() {
         return (
-            <div className="profile">
-                <h2>Profile</h2>
-                <img src={Picture} />
-                <input type="file" onChange={this.onFileSelect} />
-                <button onClick={this.onFileUpload}>Upload</button>
+            <div className="container profile">
+                <h1>Profile</h1>
+                <div className="container">
+                    <h2>Avatar</h2>
+                    <AvatarContainer />
+                </div>
                 <div className="container">
                     <h2>User details</h2>
                     {this.props.isFetchingUserDetails ? <ClipLoader /> : <UserDetailsContainer />}
