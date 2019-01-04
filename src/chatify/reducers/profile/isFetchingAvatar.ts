029ee53d2b343a32065009a594e23507cdc121e7
Stefan Svinciak <stefan.svinciak@gmail.com>
2019-01-04 20:11:19 +0100

    added fetchingAvatar reducer

---
+++ src/chatify/reducers/profile/isFetchingAvatar.ts
@@ -0,0 +1,19 @@
+import {
+    CHATIFY_PROFILE_FETCH_AVATAR_STARTED,
+    CHATIFY_PROFILE_FETCH_AVATAR_FAILED,
+    CHATIFY_PROFILE_FETCH_AVATAR_SUCCESS
+} from './../../constants/actionTypes';
+
+export const isFetchingAvatar = (prevState: boolean, action: Action): boolean => {
+    switch (action.type) {
+        case CHATIFY_PROFILE_FETCH_AVATAR_STARTED: {
+            return true;
+        }
+        case CHATIFY_PROFILE_FETCH_AVATAR_SUCCESS:
+        case CHATIFY_PROFILE_FETCH_AVATAR_FAILED: {
+            return false;
+        }
+        default:
+            return prevState;
+    }
+};
