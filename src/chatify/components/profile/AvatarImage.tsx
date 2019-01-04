3f19662a743e9d3b674e86b5dfa709e3956ff11e
Stefan Svinciak <stefan.svinciak@gmail.com>
2019-01-04 17:56:10 +0100

    added AvatarImage component for rendering image

---
+++ src/chatify/components/profile/AvatarImage.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import * as Picture from '../../images/default-profile.png';
+
+interface IAvatarImageProps {
+    url: string | null;
+}
+
+const imgStyle = {
+    width: '250px',
+    height: '250px',
+    borderRadius: '50%',
+    backgroundPosition: '50% 50%',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+};
+
+export const AvatarImage: React.SFC<IAvatarImageProps> = ({ url }) => {
+    return (
+        <img
+            style={imgStyle}
+            src={url || Picture}
+            alt="Avatar image"
+            className="avatar" />
+    );
+};
