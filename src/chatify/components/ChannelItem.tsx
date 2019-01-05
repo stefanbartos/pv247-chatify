9b0bfd83ad440281b3ffa08bb9454187c20d6183
Stefan Svinciak <stefan.svinciak@gmail.com>
2019-01-05 11:08:55 +0100

    channel ts lint fixed and removed unused bind dispatch

--- src/chatify/components/ChannelItem.tsx
+++ src/chatify/components/ChannelItem.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import {IChannelItem} from '../models/IChannelItem';
+import { IChannelItem } from '../models/IChannelItem';
+import { Link } from 'react-router-dom';
 
 export interface IChannelItemOwnProps {
     readonly id: Uuid;
@@ -10,28 +11,15 @@ export interface IChannelItemStateProps {
     readonly channel: IChannelItem;
 }
 
-export interface IChannelItemDispatchProps {
-    readonly onChannelClick: (channelId: Uuid) => void | undefined;
-}
+type IProps = IChannelItemOwnProps & IChannelItemStateProps;
 
-type IProps = IChannelItemOwnProps & IChannelItemStateProps & IChannelItemDispatchProps;
-
-export class ChannelItem extends React.PureComponent<IProps, any> {
-
-    private getChannelMessages = (event: React.MouseEvent<HTMLAnchorElement>) => {
-        const eventValue = event.target;
-        console.log(eventValue);
-        this.props.onChannelClick(this.props.id);
-    };
-
+export class ChannelItem extends React.PureComponent<IProps> {
     render() {
-        const {index, channel} = this.props;
+        const { index, channel } = this.props;
 
         return (
             <li key={index} className="nav-item">
-                <a className="nav-link" href="#" onClick={() => this.getChannelMessages}>
-                    {channel.name}
-                </a>
+                <Link to={`/channel/${channel.id}`}>{channel.name}</Link>
             </li>
         );
     }
--- src/chatify/containers/ChannelItem.tsx
+++ src/chatify/containers/ChannelItem.tsx
@@ -1,21 +1,15 @@
 import { connect } from 'react-redux';
-import { Dispatch } from 'redux';
 import { IState } from '../../common/IState';
-import { createChannel } from '../actions/actionCreators';
-import { IChannelItemDispatchProps, IChannelItemOwnProps, IChannelItemStateProps, ChannelItem } from '../components/ChannelItem';
+import { IChannelItemOwnProps, IChannelItemStateProps, ChannelItem } from '../components/ChannelItem';
 import { IChannelItem } from '../models/IChannelItem';
 
-const mapStateToProps = (state: IState, ownProps: IChannelItemOwnProps) => {
+const mapStateToProps = (state: IState, ownProps: IChannelItemOwnProps): IChannelItemStateProps => {
     return {
-        channel: state.chatify.channels.find((i: IChannelItem) => i.id === ownProps.id),
+        channel: state.chatify.channels.find((i: IChannelItem) => i.id === ownProps.id)!,
     };
 };
 
-const mapDispatchToProps = (dispatch: Dispatch) => {
-    return {
-        onCreate: (channelName: string) => dispatch(createChannel(channelName)),
-    };
-};
-
-// @ts-ignore
-export const ChannelItemContainer = connect<IChannelItemStateProps, IChannelItemDispatchProps, IChannelItemOwnProps>(mapStateToProps, mapDispatchToProps)(ChannelItem);
+export const ChannelItemContainer = connect<IChannelItemStateProps, null, IChannelItemOwnProps>(
+    mapStateToProps,
+    null
+)(ChannelItem);
