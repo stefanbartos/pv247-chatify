665cfe18f5dbd47c8fc255fe29c6c5904672ce81
Stefan Svinciak <stefan.svinciak@gmail.com>
2019-01-05 11:09:55 +0100

    refactored chatify component and added channelId from url

--- src/chatify/components/chat/Chatify.tsx
+++ src/chatify/components/chat/Chatify.tsx
@@ -1,49 +1,68 @@
 import * as React from 'react';
-// import {SendForm} from './SendForm';
-import {Title} from './Title';
+import { Title } from './Title';
 import * as Immutable from 'immutable';
+import { ChannelList } from '../ChannelList';
+import { MessageList } from './MessageList';
+import { IChatMessage } from '../../models/IChatMessage';
+import { Navigation } from '../../../common/components/Navigation';
+import { RouteComponentProps } from 'react-router';
+import { CHANNEL_ID } from '../../constants/routes';
 
-import {ChannelList} from '../ChannelList';
-import {MessageList} from './MessageList';
-import {IChatMessage} from '../../models/IChatMessage';
+interface ChatifyRouterProps {
+    match: string;
+    id?: string;
+}
+
+export interface ChatifyOwnProps extends RouteComponentProps<ChatifyRouterProps> { }
 
 export interface IChatifyStateProps {
     channelIds: Immutable.List<Uuid>;
     messageIdsList: Immutable.List<Uuid>;
 }
 
-export interface IChatifyDispatchProps  {
+export interface IChatifyDispatchProps {
     readonly onChannelAdd: (text: string) => void;
     readonly onChannelClick: (channelId: Uuid) => void;
     readonly onSendMessage: (chanelId: Uuid, chatMessage: IChatMessage) => void;
     readonly onDeleteMessage: (channelId: Uuid, chatMessageId: Uuid) => void;
 }
 
-export class Chatify extends React.PureComponent<IChatifyStateProps & IChatifyDispatchProps > {
+export class Chatify extends React.PureComponent<IChatifyStateProps & IChatifyDispatchProps & ChatifyOwnProps> {
     render() {
-        return <div id="container-fluid">
-            <div className="row">
-                <div id="sidebar" className="col-md-3">
-                    <ChannelList
-                        channelsIds={this.props.channelIds}
-                        onChannelAdd={this.props.onChannelAdd}
-                    />
-                </div>
-                <div id="messageList" className="col-md-6">
-                    <div className="container">
-                        <Title/>
+        const { match } = this.props;
+        const isChannelSelected = match.path === CHANNEL_ID;
+        return (
+            <>
+                <Navigation />
+                <div id="container">
+                    <div className="row">
+                        <div id="sidebar" className="col-md-3">
+                            <ChannelList
+                                channelsIds={this.props.channelIds}
+                                onChannelAdd={this.props.onChannelAdd}
+                            />
+                        </div>
+                        {isChannelSelected
+                            ?
+                            <div id="messageList" className="col-md-6">
+                                <div className="container">
+                                    <Title />
+                                </div>
+                                <div className="container">
+                                    <MessageList
+                                        messageIdsList={this.props.messageIdsList}
+                                        onSendMessage={this.props.onSendMessage}
+                                        channelId={match.params.id ? match.params.id : ''} />
+                                </div>
+                            </div>
+                            :
+                            <div className="col-md-5" style={{ height: '500px' }}>
+                                Select channel please
+                            </div>
+                        }
                     </div>
-                    <div className="container">
-                        <MessageList
-                            messageIdsList={this.props.messageIdsList}
-                            onSendMessage={this.props.onSendMessage}
-                            channelId={'0'}/>
-                    </div>
-                    {/*<div className="container">*/}
-                    {/*<SendForm/>*/}
-                    {/*</div>*/}
                 </div>
-            </div>
-        </div>;
+            </>
+        );
     }
 }
