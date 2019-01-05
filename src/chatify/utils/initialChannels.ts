3ac9b5f86d9fd341a77a2f360ddc29ecf8ff0205
Stefan Svinciak <stefan.svinciak@gmail.com>
2019-01-05 22:54:30 +0100

    removed not necessary initial channels

--- src/chatify/utils/initialChannels.ts
+++
@@ -1,9 +0,0 @@
-import { IChannelItem } from '../models/IChannelItem';
-import * as Immutable from 'immutable';
-import * as uuid from 'uuid';
-
-export const getInitialChannels = (): Immutable.List<IChannelItem> => Immutable.List([
-    { id: uuid(), name: 'Channel 1', author: 'string', members: Immutable.List() },
-    { id: uuid(), name: 'Channel 2', author: 'string', members: Immutable.List() },
-    { id: uuid(), name: 'Channel 3', author: 'string', members: Immutable.List() }
-]);
--- src/getInitialState.ts
+++ src/getInitialState.ts
@@ -1,10 +1,8 @@
-import { getInitialChannels } from './chatify/utils/initialChannels';
 import { getInitialMessages } from './chatify/utils/initialMessages';
 import { getPersistedToken } from './getPersistedToken';
 
 export const getInitialState = () => ({
     chatify: {
-        channels: getInitialChannels(),
         messageList: getInitialMessages(),
         token: getPersistedToken()
     }
