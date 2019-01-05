33a4f59966aeee7907b465f7e422209a1182fd89
Stefan Svinciak <stefan.svinciak@gmail.com>
2019-01-05 17:32:44 +0100

    added checking for token in privatRroute

--- src/common/components/PrivateRoute.tsx
+++ src/common/components/PrivateRoute.tsx
@@ -1,28 +1,29 @@
 import * as React from 'react';
 import { Route, RouteProps, Redirect, RouteComponentProps } from 'react-router';
 import * as routes from '../../chatify/constants/routes';
+import { getPersistedToken } from '../../getPersistedToken';
 
 interface PrivateRouteProps extends RouteProps {
     component: React.ComponentType<RouteComponentProps<any>> | React.ComponentType<any>;
 }
 
-export const PrivateRoute: React.StatelessComponent<PrivateRouteProps> = ({ component: Component, ...rest }) => (
-    <Route
-        {...rest}
-        render={componentProps =>
-            // TODO: replace false with information that user is authenticated
-            true
-                ? <Component {...componentProps} />
-                : <Redirect
-                    to={{
-                        pathname: routes.LOGIN,
-                        state: {
-                            from: componentProps.location,
-                            requiresAuth: true
-                        }
-                    }}
-                />
-        } />
-);
+export const PrivateRoute: React.StatelessComponent<PrivateRouteProps> =
+    ({ component: Component, ...rest }) => (
+        <Route
+            {...rest}
+            render={componentProps =>
+                getPersistedToken() !== null
+                    ? <Component {...componentProps} />
+                    : <Redirect
+                        to={{
+                            pathname: routes.LOGIN,
+                            state: {
+                                from: componentProps.location,
+                                requiresAuth: true
+                            }
+                        }}
+                    />
+            } />
+    );
 
 PrivateRoute.displayName = 'PrivateRoute';
