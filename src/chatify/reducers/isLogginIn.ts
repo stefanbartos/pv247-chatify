966b428f427dccd0f0bbbe924cc56b269e8f43c7
Stefan Svinciak <stefan.svinciak@gmail.com>
2019-01-20 21:48:56 +0100

    added default state for loggingIn

--- src/chatify/reducers/isLogginIn.ts
+++ src/chatify/reducers/isLogginIn.ts
@@ -4,7 +4,7 @@ import {
     CHATIFY_USER_LOGIN_FAILURE
 } from '../constants/actionTypes';
 
-export const isLoggingIn = (prevState: boolean, action: Action): boolean => {
+export const isLoggingIn = (prevState: boolean = false, action: Action): boolean => {
     switch (action.type) {
         case CHATIFY_USER_LOGIN_STARTED: {
             return true;
