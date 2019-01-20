0a1a8caa40c116fa487d3e9c15aedd3153426737
Stefan Svinciak <stefan.svinciak@gmail.com>
2019-01-20 18:09:40 +0100

    added dismiss timeout for login user error message

--- src/chatify/actions/authentication/loginUser.ts
+++ src/chatify/actions/authentication/loginUser.ts
@@ -1,7 +1,16 @@
-import { FAILED_LOGIN_MESSAGE } from './../../constants/uiMessages';
+import { dismissError } from './../error/actionCreators';
+import {
+    FAILED_LOGIN_MESSAGE,
+    MILISECONDS_TO_AUTO_DISMISS_ERROR
+} from './../../constants/uiMessages';
 import { Dispatch } from 'redux';
 import { loginApiAsync } from '../../../api/usersRepository';
-import { loginUserStarted, loginUserSuccess, loginUserFailure, receiveToken } from './actionCreators';
+import {
+    loginUserStarted,
+    loginUserSuccess,
+    loginUserFailure,
+    receiveToken
+} from './actionCreators';
 import { IAuthenticationResponse } from '../../models/IAuthenticationResponse';
 import { setAuthenticationDataToLocalStorage } from '../../../common/localStorageUtils';
 
@@ -16,6 +25,9 @@ export const loginUser = (email: string, redirect: () => void): any => async (di
         redirect();
     }
     catch (err) {
-        dispatch(loginUserFailure(FAILED_LOGIN_MESSAGE, err));
+        const dispatchedAction = dispatch(loginUserFailure(FAILED_LOGIN_MESSAGE, err));
+        setTimeout(() => {
+            dispatch(dismissError(dispatchedAction.payload.error.id));
+        }, MILISECONDS_TO_AUTO_DISMISS_ERROR);
     }
 };
