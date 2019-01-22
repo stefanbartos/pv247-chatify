12d1b6bf67bec4c67ff2579da87ba5ef9bbc0e9b
Stefan Svinciak <stefan.svinciak@gmail.com>
2019-01-22 09:03:19 +0100

    enhanced design of register page

--- src/chatify/components/register/Register.tsx
+++ src/chatify/components/register/Register.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import * as routes from '../../constants/routes';
 import { ErrorsContainer } from '../../containers/ErrorsContainer';
+import '../../css/register.css';
 
 export interface IRegisterProps {
     onRegister(email: string): void;
@@ -36,27 +37,40 @@ export class Register extends React.PureComponent<IRegisterProps, IRegisterState
     render() {
         const { email } = this.state;
         return (
-            <div className="col-md-6 offset-md-3">
-                <h1>Register</h1>
-                <form className="form-register" onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                        <label htmlFor="email">Email</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            name="email"
-                            value={email}
-                            onChange={this.handleEmailChange}
-                        />
+            <div className="container register-page h-100">
+                <div className="row justify-content-md-center h-100">
+                    <div className="card-wrapper">
+                        <div className="card fat">
+                            <div className="card-body">
+                                <h4 className="card-title">
+                                    Register
+                                </h4>
+                                <form className="form-register" onSubmit={this.onSubmit}>
+                                    <div className="form-group">
+                                        <label htmlFor="email">Email</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="email"
+                                            value={email}
+                                            onChange={this.handleEmailChange}
+                                        />
+                                    </div>
+                                    <div className="form-group m-0">
+                                        <button className="btn btn-primary btn-block">Register</button>
+                                    </div>
+                                    <div className="mt-4 text-center">
+                                        Already registered?&nbsp;
+                                        <Link to={routes.LOGIN}>Login</Link>
+                                    </div>
+                                    <div className="row">
+                                        <ErrorsContainer key="errors" />
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <button className="btn btn-primary">Register</button>
-                        Already registered? <Link to={routes.LOGIN} className="btn btn-link">Login</Link>
-                    </div>
-                    <div className="row">
-                        <ErrorsContainer key="errors" />
-                    </div>
-                </form>
+                </div>
             </div>
         );
     }
